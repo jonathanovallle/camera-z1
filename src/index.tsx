ecad259ffb8e61a8f4887c429478bd28226d9e85
Jonathan <jonathan@pinchaaqui,es>
2022-05-19 12:48:34 +0200

    feat : create component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Camera } from './components/camera';
+
+
+
+const uploadImage = async (file : any) => {
+  const formData = new FormData();
+  formData.append('file', file);
+
+};
+
+function App() {
+  return (
+      <div className="App">
+          <h1>Image capture test</h1>
+          <p>Capture image from USB webcamera and upload to form</p>
+          <Camera sendFile={uploadImage} />
+      </div>
+  );
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +31,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
